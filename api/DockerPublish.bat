9283345fa38533b87cd9be5f4c28d3dbd95ec9bd
Paul B <a@b.com>
2019-02-12 14:29:33 -0600

    AWS docker changes

--- api/DockerPublish.bat
+++ api/DockerPublish.bat
@@ -1,7 +1,9 @@
 rem aws ecr create-repository --repository-name demo-public-api
 
-docker tag demo-public-api ???.dkr.ecr.us-east-1.amazonaws.com/demo-public-api
+docker tag demo-public-api 948177242161.dkr.ecr.us-east-2.amazonaws.com/demo-public-api:latest
 aws ecr get-login --no-include-email | cmd
-docker push ???.dkr.ecr.us-east-1.amazonaws.com/demo-public-api
+docker push 948177242161.dkr.ecr.us-east-2.amazonaws.com/demo-public-api:latest
 
-aws ecs update-service --cluster Demo --service demo-public-api --force-new-deployment
+rem aws ecs update-service --cluster Demo --service demo-public-api --force-new-deployment
+
+pause
